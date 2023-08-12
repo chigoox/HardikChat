19942db5dc1b3d6d9fbafad6cef8a01e3e633769
ED <dikeemmanuel54@gmail.com>
2023-08-12 13:45:08 -0400

    added chat

---
+++ netlify/functions/ChatAI.js
@@ -0,0 +1,30 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY 
+});
+
+export const handler = async (req, res) => {
+  const body = req.body
+  const request = JSON.parse(req.body)
+  const messages = request.messages
+  
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [messages],
+});
+
+
+
+
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      Chat: response,
+    }),
+  }
+}
